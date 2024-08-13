ec6956cd9909bab53a90493cbfa440ac5fa9a6a3
warm <290631660@qq.com>
2024-08-13 18:18:36 +0800

    [update] 修改文档排版

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -39,15 +39,6 @@ export default navbar([
         link: '/common/troubleshooting.md'
     },
     {
-        text: '项目地址',
-        icon: '/icons/project.svg',
-        children: [
-            { text: 'github', icon: '/icons/github.svg', link: 'https://github.com/dromara/warm-flow.git' },
-            { text: 'gitee', icon: '/icons/gitee.svg', link: 'https://gitee.com/dromara/warm-flow.git' },
-            { text: 'gitcode', icon: '/icons/gitCode.svg', link: 'https://gitcode.com/minliuhua/warm-flow.git' },
-        ]
-    },
-    {
         text: '更新计划',
         icon: '/icons/member.svg',
         link: '/common/update.md'
