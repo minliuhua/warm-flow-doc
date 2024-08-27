84f85d9f29a686197f820c737f6dac6a676db5b5
warm <290631660@qq.com>
2024-08-27 10:17:24 +0800

    [update] 更新全局监听器案例

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -26,9 +26,9 @@ export default navbar([
         text: '历史版本',
         icon: '/icons/version.svg',
         children: [
-            { text: 'v1.2.5', link: '/v1.2.5/guide/introduction.html' },
-            { text: 'v1.2.4', link: '/v1.2.4/guide/introduction.html' },
-            { text: 'v1.2.3', link: '/v1.2.3/guide/introduction.html' },
+            { text: 'v1.2.5', link: '/v1.2.5/guide/started.md' },
+            { text: 'v1.2.4', link: '/v1.2.4/guide/started.md' },
+            { text: 'v1.2.3', link: '/v1.2.3/guide/started.md' },
         ]
     },
 ]);
--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -8,9 +8,9 @@ export default sidebar({
             collapsible: false,
             children: [
                 '/master/guide/started.md',
+                '/master/guide/processdemo.md',
                 '/master/guide/table.md',
                 '/master/guide/config.md',
-                '/master/guide/processdemo.md',
                 '/master/guide/api.md',
                 '/master/guide/datafillhandler.md',
                 '/master/guide/variable.md',
@@ -24,119 +24,12 @@ export default sidebar({
             icon: "/icons/advanced.svg",
             collapsible: false,
             children: [
+                '/master/guide/listener.md',
                 '/master/guide/logicdelete.md',
                 '/master/guide/tenant.md',
                 '/master/guide/form.md',
                 '/master/guide/customstatus.md',
-                '/master/guide/listener.md',
                 '/master/guide/expression.md',
-            ],
-        }
-    ],
-    "/v1.2.5/guide/": [
-        {
-            text: "基础部分",
-            icon: "bulb",
-            collapsible: false,
-            children: [
-                '/v1.2.5/guide/introduction.md',
-                '/v1.2.5/guide/started.md',
-                '/v1.2.5/guide/table.md',
-                '/v1.2.5/guide/config.md',
-                '/v1.2.5/guide/api.md',
-                '/v1.2.5/guide/processdemo.md',
-                '/v1.2.5/guide/processterm.md',
-                '/v1.2.5/guide/processrule.md',
-            ],
-        },
-        {
-            text: "进阶部分",
-            icon: "/icons/advanced.svg",
-            collapsible: false,
-            children: [
-                '/v1.2.5/guide/logicdelete.md',
-                '/v1.2.5/guide/tenant.md',
-                '/v1.2.5/guide/customstatus.md',
-                '/v1.2.5/guide/listener.md',
-                '/v1.2.5/guide/expression.md',
-                '/v1.2.5/guide/ormusagetips.md',
-            ],
-        }
-    ],
-    "/v1.2.3/guide/": [
-        {
-            text: "基础部分",
-            icon: "bulb",
-            collapsible: false,
-            children: [
-                '/v1.2.3/guide/introduction.md',
-                '/v1.2.3/guide/started.md',
-                '/v1.2.3/guide/table.md',
-                '/v1.2.3/guide/config.md',
-                '/v1.2.3/guide/processdemo.md',
-                '/v1.2.3/guide/processterm.md',
-                '/v1.2.3/guide/processrule.md',
-            ],
-        },
-        {
-            text: "进阶部分",
-            icon: "/icons/advanced.svg",
-            collapsible: false,
-            children: [
-                '/v1.2.3/guide/api.md',
-                '/v1.2.3/guide/listener.md',
-                '/v1.2.3/guide/expression.md',
-                '/v1.2.3/guide/ormusagetips.md',
-            ],
-        },
-        {
-            text: "其他",
-            icon: "/icons/other.svg",
-            collapsible: false,
-            children: [
-                '/v1.2.3/guide/learningmaterials.md',
-                '/v1.2.3/guide/troubleshooting.md',
-                '/v1.2.3/guide/update.md',
-            ],
-        }
-    ],
-    "/v1.2.4/guide/": [
-        {
-            text: "基础部分",
-            icon: "bulb",
-            collapsible: false,
-            children: [
-                '/v1.2.4/guide/introduction.md',
-                '/v1.2.4/guide/started.md',
-                '/v1.2.4/guide/table.md',
-                '/v1.2.4/guide/config.md',
-                '/v1.2.4/guide/api.md',
-                '/v1.2.4/guide/processdemo.md',
-                '/v1.2.4/guide/processterm.md',
-                '/v1.2.4/guide/processrule.md',
-            ],
-        },
-        {
-            text: "进阶部分",
-            icon: "/icons/advanced.svg",
-            collapsible: false,
-            children: [
-                '/v1.2.4/guide/logicdelete.md',
-                '/v1.2.4/guide/tenant.md',
-                '/v1.2.4/guide/customstatus.md',
-                '/v1.2.4/guide/listener.md',
-                '/v1.2.4/guide/expression.md',
-                '/v1.2.4/guide/ormusagetips.md',
-            ],
-        },
-        {
-            text: "其他",
-            icon: "/icons/other.svg",
-            collapsible: false,
-            children: [
-                '/v1.2.4/guide/learningmaterials.md',
-                '/v1.2.4/guide/troubleshooting.md',
-                '/v1.2.4/guide/update.md',
             ],
         }
     ],
@@ -190,5 +83,114 @@ export default sidebar({
             collapsible: false,
             link: '/common/support.md',
         },
+    ],
+    "/v1.2.5/guide/": [
+        {
+            text: "基础部分",
+            icon: "bulb",
+            collapsible: false,
+            children: [
+                '/v1.2.5/guide/started.md',
+                '/v1.2.5/guide/processdemo.md',
+                '/v1.2.5/guide/table.md',
+                '/v1.2.5/guide/config.md',
+                '/v1.2.5/guide/api.md',
+                '/v1.2.5/guide/datafillhandler.md',
+                '/v1.2.5/guide/variable.md',
+                '/v1.2.5/guide/ormusagetips.md',
+                '/v1.2.5/guide/processterm.md',
+                '/v1.2.5/guide/processrule.md',
+            ],
+        },
+        {
+            text: "进阶部分",
+            icon: "/icons/advanced.svg",
+            collapsible: false,
+            children: [
+                '/v1.2.5/guide/listener.md',
+                '/v1.2.5/guide/logicdelete.md',
+                '/v1.2.5/guide/tenant.md',
+                '/v1.2.5/guide/form.md',
+                '/v1.2.5/guide/customstatus.md',
+                '/v1.2.5/guide/expression.md',
+            ],
+        }
+    ],
+    "/v1.2.4/guide/": [
+        {
+            text: "基础部分",
+            icon: "bulb",
+            collapsible: false,
+            children: [
+                '/v1.2.4/guide/introduction.md',
+                '/v1.2.4/guide/started.md',
+                '/v1.2.4/guide/table.md',
+                '/v1.2.4/guide/config.md',
+                '/v1.2.4/guide/api.md',
+                '/v1.2.4/guide/processdemo.md',
+                '/v1.2.4/guide/processterm.md',
+                '/v1.2.4/guide/processrule.md',
+            ],
+        },
+        {
+            text: "进阶部分",
+            icon: "/icons/advanced.svg",
+            collapsible: false,
+            children: [
+                '/v1.2.4/guide/logicdelete.md',
+                '/v1.2.4/guide/tenant.md',
+                '/v1.2.4/guide/customstatus.md',
+                '/v1.2.4/guide/listener.md',
+                '/v1.2.4/guide/expression.md',
+                '/v1.2.4/guide/ormusagetips.md',
+            ],
+        },
+        {
+            text: "其他",
+            icon: "/icons/other.svg",
+            collapsible: false,
+            children: [
+                '/v1.2.4/guide/learningmaterials.md',
+                '/v1.2.4/guide/troubleshooting.md',
+                '/v1.2.4/guide/update.md',
+            ],
+        }
+    ],
+    "/v1.2.3/guide/": [
+        {
+            text: "基础部分",
+            icon: "bulb",
+            collapsible: false,
+            children: [
+                '/v1.2.3/guide/introduction.md',
+                '/v1.2.3/guide/started.md',
+                '/v1.2.3/guide/table.md',
+                '/v1.2.3/guide/config.md',
+                '/v1.2.3/guide/processdemo.md',
+                '/v1.2.3/guide/processterm.md',
+                '/v1.2.3/guide/processrule.md',
+            ],
+        },
+        {
+            text: "进阶部分",
+            icon: "/icons/advanced.svg",
+            collapsible: false,
+            children: [
+                '/v1.2.3/guide/api.md',
+                '/v1.2.3/guide/listener.md',
+                '/v1.2.3/guide/expression.md',
+                '/v1.2.3/guide/ormusagetips.md',
+            ],
+        },
+        {
+            text: "其他",
+            icon: "/icons/other.svg",
+            collapsible: false,
+            children: [
+                '/v1.2.3/guide/learningmaterials.md',
+                '/v1.2.3/guide/troubleshooting.md',
+                '/v1.2.3/guide/update.md',
+            ],
+        }
     ],
 });
