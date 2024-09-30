8eba6ebb9290a3a6c31991238e4dc83cc0f67df4
warm <290631660@qq.com>
2024-09-30 14:51:04 +0800

    [update] 更新特别赞助

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -12,7 +12,7 @@ export default defineUserConfig({
         "/": {
             lang: "zh-CN",
             title: "warm-flow工作流",
-            description: "国产自研工作流，其特点简洁(只有6张表)但又不简单，五脏俱全，组件独立，可扩展，可满足中小项目的组件。",
+            description: "国产工作流引擎🎉，其特点简洁轻量但又不简单，五脏俱全，组件独立，可扩展，可满足中小项目的组件。",
         },
     },
     theme,
