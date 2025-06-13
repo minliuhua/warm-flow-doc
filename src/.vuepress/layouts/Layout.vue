1dbb4456ab8e813d1bb340ebaca67197a99316b7
warm <290631660@qq.com>
2025-06-13 10:56:42 +0800

    [update] 复制流程对象，不需要复制id、createTime和updateTime

--- src/.vuepress/layouts/Layout.vue
+++ src/.vuepress/layouts/Layout.vue
@@ -90,12 +90,6 @@ watch(
 
 <template>
   <Layout>
-    <template v-if="frontmatter.home" #heroBefore>
-      <div ref="container" class="w-full h-[calc(100vh-88px)]">
-        <iframe ref="iframe" src="http://localhost:81/warm-flow-ui/index.html" width="1800px" height="800px"></iframe>
-      </div>
-    </template>
-
     <template v-if="!frontmatter.home" #sidebarTop>
       <div v-html="sidebarContentLift" />
     </template>
